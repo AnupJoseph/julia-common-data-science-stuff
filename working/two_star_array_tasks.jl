45e20c089e77f3367f71ce1558871bdc751db59a
AnupJoseph <anup20joseph@gmail.com>
2021-12-02 18:35:07 +0530

    Added a few 2 star questions

--- working/two_star_array_tasks.jl
+++ working/two_star_array_tasks.jl
@@ -16,7 +16,24 @@ B = 2ones(3)
 
 
 # Extract the integer part of a random array of positive numbers?
-Z = rand(0:0.01:100,20)
-make_int(x) = trunc(Int,x)
+Z = rand(0:0.01:100, 20)
+make_int(x) = trunc(Int, x)
 make_int.(Z)
 # Now the question says 4 ways but Julia has a nice standard way of dealing with this so I am sticking to it
+
+# Create a 5x5 matrix with row values ranging from 0 to 4 
+A = fill([0:4;], 5)
+# This is the solution if you want a array of arrays
+
+# The solution for this particular problem
+A = [0 1 2 3 4]
+repeat(A, 5)
+
+
+# This is a more generic solution but slightly more involved
+repeat(reshape([0:4;], 1, :), 5)
+# Lets break down as so 
+# * [0:4;] creates a vector [0,1,2,3,4]
+# * reshape([0:4;], 1, :) converts to 1x5 row vector from the column vector
+# * The repeat function basically copies it five times
+
