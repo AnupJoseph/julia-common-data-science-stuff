82ff0e6d0f61106642241d9c73db3a73d1ede269
AnupJoseph <anup20joseph@gmail.com>
2022-01-04 18:43:21 +0530

    Finished chipotle excercise

--- working/dataframe_tasks/01_Getting_&_Knowing_Your_Data/Chipotle/with_dataframes.jl
+++ working/dataframe_tasks/01_Getting_&_Knowing_Your_Data/Chipotle/with_dataframes.jl
@@ -1,13 +1,15 @@
 # Step 1. Import the necessary libraries
 using CSV
 using DataFrames
+using Pipe
+using Statistics
 
 # Step 2. Import the dataset from this (https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv).
 URL = "https://raw.githubusercontent.com/justmarkham/DAT8/master/data/chipotle.tsv"
 download(URL, "chipotle.tsv")
 
 # Step 3. Assign it to a variable called chipo.
-chipo = CSV.read("chipotle.tsv", DataFrame, delim = "\t")
+chipo = CSV.read("chipotle.tsv", DataFrame, delim = "\t", missingstring = missing)
 
 # Step 4. See the first 10 entries
 first(chipo, 10)
@@ -22,5 +24,27 @@ ncol(chipo)
 names(chipo)
 
 # Step 9. Which was the most-ordered item?
-gchipo = groupby(chipo, :item_name)
-combine(gchipo,sum,sort)
+chipo_groupby = @pipe chipo |> groupby(_, :item_name) |> combine(_, :quantity => sum => :total_quantity)
+
+sort(chipo_groupby, :total_quantity, rev = true)
+
+replace!(chipo[:choice_description], "" => missing)
+
+# Step 11. What was the most ordered item in the choice_description column?
+choice_description_groupby = @pipe chipo |> groupby(_, :choice_description) |> combine(_, :quantity => sum => :total_quantity)
+
+sort(choice_description_groupby, :total_quantity, rev = true)
+
+# Step 12. How many items were orderd in total?
+sum(chipo[:quantity])
+
+# Step 13. Turn the item price into a float
+transform!(chipo, :item_price => ByRow(x -> parse(Float64, x[2:end])) => :item_price)
+
+# Step 14. How much was the revenue for the period in the dataset?
+sum(chipo.quantity .* chipo.item_price)
+
+# Step 15. How many orders were made in the period?
+order_counts = nrow(combine(groupby(chipo, :order_id), nrow => :count_per_order))
+
+# Step 16. What is the average revenue amount per order?
