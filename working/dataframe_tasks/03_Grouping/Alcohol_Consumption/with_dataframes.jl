e3297a1889a68891d92803616ed95aac654d899a
AnupJoseph <anup20joseph@gmail.com>
2022-01-22 18:46:23 +0530

    Added Julia Code

---
+++ working/dataframe_tasks/03_Grouping/Alcohol_Consumption/with_dataframes.jl
@@ -0,0 +1,27 @@
+# Step 1. Import the necessary libraries
+using DataFrames
+using CSV
+using Pipe
+using Statistics
+
+# Step 2. Import the dataset from this [address](https://raw.githubusercontent.com/justmarkham/DAT8/master/data/drinks.csv).
+URL = "https://raw.githubusercontent.com/justmarkham/DAT8/master/data/drinks.csv"
+download(URL, "drinks.csv")
+
+# Step 3. Assign it to a variable called drinks.
+drinks = CSV.read("drinks.csv", DataFrame)
+
+# Step 4. Which continent drinks more beer on average?
+continents = @pipe drinks |> groupby(_, :continent) |> combine(_, :beer_servings => mean)
+
+# Step 5. For each continent print the statistics for wine consumption.
+# Can't figure out a way now will get back to this at some point
+
+# Step 6. Print the mean alcohol consumption per continent for every column
+continents = @pipe drinks |> groupby(_, :continent) |> combine(_, valuecols(_)[2:end] .=> mean)
+
+# Step 7. Print the median alcohol consumption per continent for every column
+@pipe drinks |> groupby(_, :continent) |> combine(_, valuecols(_)[2:end] .=> median)
+
+# Step 8. Print the mean, min and max values for spirit consumption.
+@pipe drinks |> groupby(_, :continent) |> combine(_, :spirit_servings => (mean, min, max))
